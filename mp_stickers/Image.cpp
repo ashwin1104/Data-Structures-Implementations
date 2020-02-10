746cd6950c899be624d82ee7cfe87d4d5eadf6a7
ashwin1104 <ashwin.saxena1@gmail.com>
2020-02-10 16:39:27 -0600

    Implemented Image class (Part 1)

---
+++ mp_stickers/Image.cpp
@@ -0,0 +1,173 @@
+#include "Image.h"
+#include <string>
+#include <iostream>
+#include <cmath>
+
+void Image::lighten() {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).l+0.1 <= 1.0) {
+        this->getPixel(x,y).l += 0.1;
+      }
+      else {
+        this->getPixel(x,y).l = 1.0;
+      }
+    }
+  }
+}
+void Image::lighten(double amount) {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).l+amount <= 1.0) {
+        this->getPixel(x,y).l += amount;
+      }
+      else {
+        this->getPixel(x,y).l = 1.0;
+      }
+    }
+  }
+}
+void Image::darken() {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).l-0.1 >= 0) {
+        this->getPixel(x,y).l -= 0.1;
+      }
+      else {
+        this->getPixel(x,y).l = 0.0;
+      }
+    }
+  }
+}
+void Image::darken(double amount) {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).l-amount >= 0) {
+        this->getPixel(x,y).l -= amount;
+      }
+      else {
+        this->getPixel(x,y).l = 0.0;
+      }
+    }
+  }
+}
+void Image::saturate() {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).s+0.1 <= 1.0) {
+        this->getPixel(x,y).s += 0.1;
+      }
+      else {
+        this->getPixel(x,y).s = 1.0;
+      }
+    }
+  }
+}
+void Image::saturate(double amount) {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).s+amount <= 1.0) {
+        this->getPixel(x,y).s += amount;
+      }
+      else {
+        this->getPixel(x,y).s = 1.0;
+      }
+    }
+  }
+}
+void Image::desaturate() {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).s-0.1 >= 0.0) {
+        this->getPixel(x,y).s -= 0.1;
+      }
+      else {
+        this->getPixel(x,y).s = 0.0;
+      }
+    }
+  }
+}
+void Image::desaturate(double amount) {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      if (this->getPixel(x,y).s-amount >= 0.0) {
+        this->getPixel(x,y).s -= amount;
+      }
+      else {
+        this->getPixel(x,y).s = 0.0;
+      }
+    }
+  }
+}
+void Image::grayscale() {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      this->getPixel(x,y).s = 0.0;
+    }
+  }
+}
+void Image::rotateColor(double degrees) {
+  for (unsigned int x = 0; x < this->width(); x++) {
+    for (unsigned int y = 0; y < this->height(); y++) {
+      this->getPixel(x,y).h += degrees;
+      if (this->getPixel(x,y).h > 360) {
+        this->getPixel(x,y).h -= 360;
+      }
+    }
+  }
+}
+void Image::illinify() {
+  double illini_orange = 11;
+  double illini_blue = 216;
+  double distance_orange;
+  double distance_blue;
+  for (unsigned x = 0; x < this->width(); x++) {
+    for (unsigned y = 0; y < this->height(); y++) {
+      cs225::HSLAPixel &pixel = this->getPixel(x,y);
+      distance_orange = abs(pixel.h - illini_orange);
+      distance_blue = abs(pixel.h - illini_blue);
+      if (abs(pixel.h - 360 - illini_orange) < distance_orange) {
+        distance_orange = abs(pixel.h - 360 - illini_orange);
+      }
+      if (abs(pixel.h - 360 - illini_blue) < distance_blue) {
+        distance_blue = abs(pixel.h - 360 - illini_blue);
+      }
+      if (distance_orange/distance_blue > 1) {
+        pixel.h = illini_blue;
+      }
+      else {
+        pixel.h = illini_orange;
+      }
+    }
+  }
+}
+void Image::scale(double factor) {
+  unsigned int height = this->height() * factor;
+  unsigned int width = this->width() * factor;
+  double xreverse = (this->width())/((double) width);
+  double yreverse = (this->height())/((double) height);
+  if (xreverse > yreverse) {
+    xreverse = yreverse;
+  }
+  else {
+    yreverse = xreverse;
+  }
+  cs225::PNG old(*this);
+  this->resize(width, height);
+  for (unsigned int x = 0; x < width; x++) {
+    for (unsigned int y = 0; y < height; y++) {
+      this->getPixel(x,y) = old.getPixel((int)x*xreverse,(int)y*yreverse);
+    }
+  }
+}
+void Image::scale(unsigned w, unsigned h) {
+  double xfactor = (double) w/this->width();
+  double yfactor = (double) w/this->height();
+  double factor;
+  if (xfactor > yfactor) {
+    scale(yfactor);
+  }
+  else {
+    scale(xfactor);
+  }
+}
