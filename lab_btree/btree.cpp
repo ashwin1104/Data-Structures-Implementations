713c22508c2df0adfbaa32f0f6e224a78e66927f
ashwin1104 <ashwin.saxena1@gmail.com>
2020-03-28 04:17:47 -0500

    fix

--- lab_btree/btree.cpp
+++ lab_btree/btree.cpp
@@ -25,18 +25,21 @@ V BTree<K, V>::find(const K &key) const
 template <class K, class V>
 V BTree<K, V>::find(const BTreeNode *subroot, const K &key) const
 {
+    if (!subroot || subroot->elements.empty())
+    {
+        return V();
+    }
+
     size_t first_larger_idx = insertion_idx(subroot->elements, key);
 
-    /* If first_larger_idx is a valid index and the key there is the key we
-     * are looking for, we are done. */
-    if (subroot->elements.empty() != true && !(first_larger_idx >= subroot->elements.size()))
+    if (first_larger_idx < subroot->elements.size())
     {
-        if (subroot->elements[first_larger_idx].key == key)
+        if (subroot->elements[first_larger_idx] == key)
         {
             return subroot->elements[first_larger_idx].value;
         }
     }
-    if (subroot->is_leaf == true)
+    if (subroot->is_leaf)
     {
         return V();
     }
@@ -44,42 +47,7 @@ V BTree<K, V>::find(const BTreeNode *subroot, const K &key) const
     {
         return find(subroot->children[first_larger_idx], key);
     }
-
-    /* Otherwise, we need to figure out which child to explore. For this we
-     * can actually just use first_larger_idx directly. E.g.
-     * | 1 | 5 | 7 | 8 |
-     * Suppose we are looking for 6. first_larger_idx is 2. This means we want to
-     * explore the child between 5 and 7. The children vector has a pointer for
-     * each of the horizontal bars. The index of the horizontal bar we want is
-     * 2, which is conveniently the same as first_larger_idx. If the subroot is
-     * a leaf and we didn't find the key in it, then we have failed to find it
-     * anywhere in the tree and return the default V.
-     */
-
     return V();
-    // if (!subroot || subroot->elements.empty())
-    // {
-    //     return V();
-    // }
-
-    // size_t first_larger_idx = insertion_idx(subroot->elements, key);
-
-    // if (first_larger_idx < subroot->elements.size())
-    // {
-    //     if (subroot->elements[first_larger_idx] == key)
-    //     {
-    //         return subroot->elements[first_larger_idx].value;
-    //     }
-    //     else if (subroot->is_leaf)
-    //     {
-    //         return V();
-    //     }
-    //     else
-    //     {
-    //         return find(subroot->children[first_larger_idx], key);
-    //     }
-    // }
-    // return V();
 }
 
 /**
